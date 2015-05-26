91b499f76d106fdc8aa2a9850fd3f394e313d0e4
sumanthio <sumanthp544@gmail.com>
2015-05-26 12:29:18 +0530

    Contact route

--- src/app/index.js
+++ src/app/index.js
@@ -7,9 +7,17 @@ angular.module('portfolioSumanth', ['ngRoute', 'ngMaterial'])
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       })
-      .when('/swipe', {
-        templateUrl: 'app/main/main.html',
-        controller: 'SwipeCtrl'
+      .when('/blog', {
+        templateUrl: 'app/blog/blog.html',
+        controller: 'BlogCtrl'
+      })
+      .when('/work', {
+        templateUrl: 'app/work/work.html',
+        controller: 'WorkCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'app/contact/contact.html',
+        controller: 'ContactCtrl'
       })
       .otherwise({
         redirectTo: '/'
