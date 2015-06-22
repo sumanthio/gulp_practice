998a05d884c597614b4b9fc7328f79629e283ac4
sumanthio <sumanthp544@gmail.com>
2015-06-22 15:17:06 +0530

    New system

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -5,12 +5,30 @@
     .module('portfolioSumanth')
     .config(routeConfig);
 
-  function routeConfig($routeProvider) {
+  function routeConfig($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $locationProvider.hashPrefix = '!';
+
     $routeProvider
       .when('/', {
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .when('/work', {
+        templateUrl: 'app/work/work.html',
+        controller: 'WorkController',
+        controllerAs: 'work'
+      })
+      .when('/blog', {
+        templateUrl: 'app/blog/blog.html',
+        controller: 'BlogController',
+        controllerAs: 'blog'
+      })
+      .when('/contact', {
+        templateUrl: 'app/contact/contact.html',
+        controller: 'ContactController',
+        controllerAs: 'contact'
       })
       .otherwise({
         redirectTo: '/'
